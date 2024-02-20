d1141dce9914632e5517897d61ad44172f15d0a1
conor <fc.chelsea11@yandex.ru>
2024-02-20 22:19:31 +0300

    исправлено анотация в классе Stats.java

--- stats/stats-server/src/main/java/ru/practicum/stats/server/model/Stats.java
+++ stats/stats-server/src/main/java/ru/practicum/stats/server/model/Stats.java
@@ -1,16 +1,14 @@
 package ru.practicum.stats.server.model;
 
-import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.Data;
-import lombok.NoArgsConstructor;
+import lombok.*;
 
 import javax.persistence.*;
 import java.time.LocalDateTime;
 
 @Entity
 @Table(name = "stats", schema = "public")
-@Data
+@Getter
+@Setter
 @AllArgsConstructor
 @NoArgsConstructor
 @Builder
